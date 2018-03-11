0fd2448cf28d59750c0c8ab4c06d6c27660a66d9
Raphael Cockx <raphael@cockx.com>
2018-03-11 17:17:10 +0100

    Use vue.config.js for defaults

--- prompts.js
+++ prompts.js
@@ -1,7 +1,17 @@
-module.exports = [
+const path = require('path')
+const vueConfig = require(path.join(process.cwd(), 'vue.config.js')).pluginOptions.meta
+
+var prompts = [
   {
     type: 'input',
     name: 'projectName',
     message: 'Please enter the name of your project:'
   }
 ]
+
+for (var key in vueConfig) {
+  var promptIndex = prompts.findIndex(question => question.name === key)
+  if (promptIndex !== -1) prompts[promptIndex].default = vueConfig[key]
+}
+
+module.exports = prompts
